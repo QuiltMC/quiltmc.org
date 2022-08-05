31b474c85fb15ae2bce0026799ded74e5edf2468
[object Object] <object-object@outlook.com>
2022-08-05 10:13:38 -0700

    Update dependancies and remove ones that aren't being used (I might be wrong about this but they can be added back later)

--- astro.config.ts
+++ astro.config.ts
@@ -1,22 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import astroI18next from "./ersatz/astro-i18next-fluent";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://quiltmc.org",
-    integrations: [
-        mdx(),
-        astroI18next({
-            baseLocale: "en",
-            supportedLocales: ["en", "zh"],
-            i18next: {
-                debug: true, // convenient during development to check for missing keys
-            },
-            i18nextPlugins: {
-                fluent: "i18next-fluent"
-            }
-        }),
-    ],
+	site: "https://quiltmc.org",
+	integrations: [
+		mdx(),
+		astroI18next({
+			baseLocale: "en",
+			supportedLocales: ["en", "zh"],
+			i18next: {
+				debug: true, // convenient during development to check for missing keys
+			},
+			i18nextPlugins: {
+				fluent: "i18next-fluent",
+			},
+		}),
+	],
 });
-
