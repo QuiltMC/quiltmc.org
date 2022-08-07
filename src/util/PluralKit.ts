95c82875ec38bbdaf7f71180083611c2a45336e4
[object Object] <object-object@outlook.com>
2022-08-07 15:28:04 -0700

    Fix bug with teams page and t a b s

--- src/util/PluralKit.ts
+++ src/util/PluralKit.ts
@@ -5,39 +5,51 @@ import { DateTime } from "luxon";
 const CACHE: Map<string, Member[]> = new Map();
 
 export interface RawMember {
-    name: string,
-    avatar_url: string,
-    created: string,
+	name: string;
+	avatar_url: string;
+	created: string;
 }
 
 export interface Member {
-    name: string,
-    avatarUrl: string,
+	name: string;
+	avatarUrl: string;
 }
 
-export async function getPKSystemMembers(memberList: string): Promise<Member[]> {
-    if (memberList.startsWith("pk:")) {
-        const id = memberList.slice("pk:".length);
+let last_request = new Date();
 
-        if (CACHE.has(id)) {
-            return CACHE.get(id)
-        }
+export async function getPKSystemMembers(
+	memberList: string
+): Promise<Member[]> {
+	if (memberList.startsWith("pk:")) {
+		const id = memberList.slice("pk:".length);
 
-        const response = await fetch(`https://api.pluralkit.me/v2/systems/${id}/members`);
-        const data: RawMember[] = await response.json();
-        
-        // TODO add custom ordering
-        const result = sortBy(data, mem => {
-            const date = DateTime.fromISO(mem.created);
-            return date.isValid ? date : DateTime.fromISO("0000-01-01T00:00:00+0000");
-        }).map(raw => {
-            return {
-                name: raw.name,
-                avatarUrl: raw.avatar_url,
-            }
-        });
-        CACHE.set(id, result);
-        return result;
-    }
-    return [];
+		if (CACHE.has(id)) {
+			return CACHE.get(id);
+		}
+
+		// The most scuffed rate limiting code in existance
+		while (new Date().getTime() - last_request.getTime() < 500) {
+			await new Promise((resolve) => setTimeout(resolve, 100));
+		}
+		last_request = new Date();
+
+		const response = await fetch(
+			`https://api.pluralkit.me/v2/systems/${id}/members`
+		);
+		const data: RawMember[] = await response.json();
+
+		// TODO add custom ordering
+		const result = sortBy(data, (mem) => {
+			const date = DateTime.fromISO(mem.created);
+			return date.isValid ? date : DateTime.fromISO("0000-01-01T00:00:00+0000");
+		}).map((raw) => {
+			return {
+				name: raw.name,
+				avatarUrl: raw.avatar_url,
+			};
+		});
+		CACHE.set(id, result);
+		return result;
+	}
+	return [];
 }
--- src/util/Util.tsx
+++ src/util/Util.tsx
@@ -1,21 +1,22 @@
 // thanks JS, way to go
 export function sortBy<T, K>(arr: T[], by: (elem: T) => K): T[] {
-    arr.sort((a, b) => {
-        const aKey = by(a);
-        const bKey = by(b);
-        if (aKey < bKey) return -1;
-        if (aKey > bKey) return 1;
-        return 0;
-    });
-    return arr;
+	arr.sort((a, b) => {
+		const aKey = by(a);
+		const bKey = by(b);
+		if (aKey < bKey) return -1;
+		if (aKey > bKey) return 1;
+		return 0;
+	});
+	return arr;
 }
+
 export function sortByDescending<T, K>(arr: T[], by: (elem: T) => K): T[] {
-    arr.sort((a, b) => {
-        const aKey = by(a);
-        const bKey = by(b);
-        if (aKey < bKey) return 1;
-        if (aKey > bKey) return -1;
-        return 0;
-    });
-    return arr;
+	arr.sort((a, b) => {
+		const aKey = by(a);
+		const bKey = by(b);
+		if (aKey < bKey) return 1;
+		if (aKey > bKey) return -1;
+		return 0;
+	});
+	return arr;
 }
