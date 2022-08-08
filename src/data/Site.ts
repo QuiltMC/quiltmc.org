181eef584011c4cc0b3d0a473853ad46b10708df
[object Object] <object-object@outlook.com>
2022-08-08 12:08:22 -0700

    more  t a b

--- src/data/Site.ts
+++ src/data/Site.ts
@@ -1,75 +1,75 @@
 export class SocialMediaHandle {
-    base: string;
-    username: string;
-    link: URL;
-    
-    constructor(base: string, username: string) {
-        this.base = base;
-        this.username = username;
-        this.link = new URL(this.username, this.base);
-    }
+	base: string;
+	username: string;
+	link: URL;
+
+	constructor(base: string, username: string) {
+		this.base = base;
+		this.username = username;
+		this.link = new URL(this.username, this.base);
+	}
 }
 
 export interface Config {
-    title: string,
-    shortTitle: string,
-    description: string,
-    url: string,
-    source: string,
-    permalink: 'pretty',
-    author: string,
+	title: string;
+	shortTitle: string;
+	description: string;
+	url: string;
+	source: string;
+	permalink: "pretty";
+	author: string;
 
-    // Social media handles
-    discordCommunity: URL,
-    discordToolchain: URL,
-    forum: URL,
-    github: SocialMediaHandle,
-    gitlab: SocialMediaHandle,
-    mastodon: SocialMediaHandle,
-    rss: URL,
-    twitch: SocialMediaHandle,
-    twitter: {
-        handle: SocialMediaHandle,
-        card: string,
-    },
-    logo: string,
-    social: {
-        name: string,
-        links: URL[]
-    }
+	// Social media handles
+	discordCommunity: URL;
+	discordToolchain: URL;
+	forum: URL;
+	github: SocialMediaHandle;
+	gitlab: SocialMediaHandle;
+	mastodon: SocialMediaHandle;
+	rss: URL;
+	twitch: SocialMediaHandle;
+	twitter: {
+		handle: SocialMediaHandle;
+		card: string;
+	};
+	logo: string;
+	social: {
+		name: string;
+		links: URL[];
+	};
 }
 
 const config: Config = {
-    title: "The Quilt Project",
-    shortTitle: "QuiltMC",
-    description: "The mod-loader that cares.",
-    url: "https://quiltmc.org",
-    source: "src/",
-    permalink: "pretty",
-    author: "QuiltMC",
+	title: "The Quilt Project",
+	shortTitle: "QuiltMC",
+	description: "The mod-loader that cares.",
+	url: "https://quiltmc.org",
+	source: "src/",
+	permalink: "pretty",
+	author: "QuiltMC",
 
-    discordCommunity: new URL("https://discord.quiltmc.org/"),
-    discordToolchain: new URL("https://discord.quiltmc.org/toolchain/"),
-    forum: new URL("https://forum.quiltmc.org/"),
-    github: new SocialMediaHandle("https://github.com/", "QuiltMC"),
-    gitlab: new SocialMediaHandle("https://gitlab.com/", "quiltmc"),
-    mastodon: new SocialMediaHandle("https://tech.lgbt/", "@quiltmc"),
-    rss: new URL("https://quiltmc.org/feed.xml"),
-    twitch: new SocialMediaHandle("https://twitch.tv/", "quiltmc"),
-    twitter: {
-        handle: new SocialMediaHandle("https://twitter.com/", "quilt_mc"),
-        card: "summary",
-    },
-    logo: '/assets/img/logo-square.png',
-    social: {
-        name: 'QuiltMC',
-        links: [
-            new URL('https://twitter.com/quilt_mc'),
-            new URL('https://github.com/QuiltMC'),
-            new URL('https://twitch.tv/quiltmc'),
-        ]
-    }
-}
+	discordCommunity: new URL("https://discord.quiltmc.org/"),
+	discordToolchain: new URL("https://discord.quiltmc.org/toolchain/"),
+	forum: new URL("https://forum.quiltmc.org/"),
+	github: new SocialMediaHandle("https://github.com/", "QuiltMC"),
+	gitlab: new SocialMediaHandle("https://gitlab.com/", "quiltmc"),
+	mastodon: new SocialMediaHandle("https://tech.lgbt/", "@quiltmc"),
+	rss: new URL("https://quiltmc.org/feed.xml"),
+	twitch: new SocialMediaHandle("https://twitch.tv/", "quiltmc"),
+	twitter: {
+		handle: new SocialMediaHandle("https://twitter.com/", "quilt_mc"),
+		card: "summary",
+	},
+	logo: "/assets/img/logo-square.png",
+	social: {
+		name: "QuiltMC",
+		links: [
+			new URL("https://twitter.com/quilt_mc"),
+			new URL("https://github.com/QuiltMC"),
+			new URL("https://twitch.tv/quiltmc"),
+		],
+	},
+};
 
 export default config;
 
