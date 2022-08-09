452b53606519a3eaa2fb02474a69af5f6e5dde7b
leocth <leocth31@gmail.com>
2022-08-09 16:55:03 +0800

    more changes to RTL settings, localize more

---
+++ src/data/RtlSettings.ts
@@ -0,0 +1,39 @@
+import { t } from "i18next";
+
+export type TextDirection = 'rtl' | 'ltr';
+export type Direction = 'left' | 'right';
+
+export interface RtlSettings {
+    textDirection: TextDirection;
+    rtl: boolean;
+    left: Direction;
+    right: Direction;
+}
+
+export default (): RtlSettings => {
+    const textDirection = t("text-direction") as TextDirection;
+    const rtl = textDirection == 'rtl';
+    return {
+        textDirection,
+        rtl,
+        left: rtl ? 'right' : 'left',
+        right: rtl ? 'left' : 'right'
+    }
+}
+
+
+// {% capture text-direction %}{% t settings.text-direction %}{% endcapture %}
+// {% capture title-separator %}{% t settings.title-separator %}{% endcapture %}
+// {% capture format-date %}{% t settings.formats.date %}{% endcapture %}
+
+
+// {% capture translated-description %}{% t page.description %}{% endcapture %}
+// {% assign description-count = translated-description | size %}
+
+// {% if description-count > 0 %}
+//     {% assign description = translated-description %}
+// {% elsif page.description %}
+//     {% assign description = page.description %}
+// {% else %}
+//     {% assign description = site.description %}
+// {% endif %}
