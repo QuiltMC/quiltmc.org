8061191f7106c23968102538b467735dcf701214
pluiedev <hi@pluie.me>
2022-11-28 15:38:58 +0800

    add PurgeCSS to remove more than 80% of served CSS

--- astro.config.ts
+++ astro.config.ts
@@ -3,11 +3,22 @@ import mdx from "@astrojs/mdx";
 import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
 import htmlMinifier from "astro-html-minifier";
+import purgecss from "astro-purgecss";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://quiltmc.org",
-	integrations: [mdx(), astroI18next(), sitemap(), htmlMinifier()],
+	integrations: [
+		mdx(),
+		astroI18next(),
+		sitemap(),
+		htmlMinifier(),
+		purgecss({
+			safelist: {
+				greedy: [/\.highlight/],
+			},
+		}),
+	],
 	markdown: {
 		syntaxHighlight: "prism",
 	},
