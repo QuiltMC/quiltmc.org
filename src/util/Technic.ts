40943ce1dca011b72376f34c85441e24cbbc59e7
pluiedev <hi@pluie.me>
2022-12-05 11:17:12 +0800

    fix Technic Launcher version downloads

--- src/util/Technic.ts
+++ src/util/Technic.ts
@@ -1,4 +1,4 @@
-import { loadGameVersions, GameVersion } from "./Meta";
+import { loadGameVersions, GameVersion, LoaderVersion } from "./Meta";
 
 const loaderTarget = document.getElementById(
 	"loader-select"
@@ -10,65 +10,59 @@ const buttonTarget = document.getElementById(
 	"button-target"
 ) as HTMLAnchorElement;
 
-let gameVersions: GameVersion[] = [];
-
 async function populateMinecraftVersions() {
 	const vsns = await loadGameVersions();
-	gameVersions = vsns;
 	const select = minecraftTarget;
 	select.innerHTML = "";
 
-	let sel = -1;
-	let idx = 0;
-	for (const v of vsns) {
+	let options = vsns.map((v) => {
 		const opt = document.createElement("option");
 		opt.value = v.name;
 		opt.textContent = v.name;
-		select.appendChild(opt);
-		if (sel === -1 && v.stable) {
-			sel = idx;
-		}
-		idx++;
-	}
-	select.selectedIndex = sel;
+		return opt;
+	});
+	select.append(...options);
+	select.oninput = () => {
+		populateLoaderVersions(vsns[select.selectedIndex]);
+	};
+	select.selectedIndex = vsns.findIndex((v) => v.stable);
+	await populateLoaderVersions(vsns[select.selectedIndex]);
 
-	await populateLoaderVersions();
 	minecraftTarget.removeAttribute("disabled");
 }
 
-async function populateLoaderVersions() {
+async function populateLoaderVersions(gameVersion: GameVersion) {
 	loaderTarget.setAttribute("disabled", "");
 	buttonTarget.setAttribute("disabled", "");
 	buttonTarget.removeAttribute("download");
-	buttonTarget.classList.remove("is-primary");
 
-	const mcSel = minecraftTarget;
-	const mcVer = gameVersions[mcSel.selectedIndex];
-
-	const vsns = await mcVer.getLoaderVersions();
+	const vsns = await gameVersion.getLoaderVersions();
 
 	const select = loaderTarget;
 	select.innerHTML = "";
 
-	for (const v of vsns) {
+	let options = vsns.map((v) => {
 		const opt = document.createElement("option");
 		opt.value = v.version;
 		opt.textContent = v.version;
-		select.appendChild(opt);
-	}
+		return opt;
+	});
+	select.append(...options);
 
 	select.selectedIndex = 0;
-
+	select.oninput = () => {
+		populateButton(gameVersion, vsns[select.selectedIndex]);
+	};
+	await populateButton(gameVersion, vsns[select.selectedIndex]);
 	loaderTarget.removeAttribute("disabled");
-
-	await populateButton();
 }
 
-async function populateButton() {
-	const mcVersion = minecraftTarget.selectedOptions[0];
-	const loaderVersion = loaderTarget.selectedOptions[0];
+async function populateButton(
+	gameVersion: GameVersion,
+	loaderVersion: LoaderVersion
+) {
+	buttonTarget.href = `https://meta.quiltmc.org/v3/versions/loader/${gameVersion.name}/${loaderVersion.version}/profile/json`;
 
-	buttonTarget.href = `https://meta.quiltmc.org/v3/versions/loader/${mcVersion.text}/${loaderVersion.text}/profile/json`;
 	buttonTarget.setAttribute("download", "version.json");
 
 	buttonTarget.removeAttribute("disabled");
