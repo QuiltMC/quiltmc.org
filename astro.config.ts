c9a72f20bed45f002791ab70f6404aca477dbe62
Ambre Bertucci <ambre@akarys.me>
2023-03-10 22:36:40 +0000

    Start working on a header anchor
    Display a link icon when hovering headers with a jump link to that header.
    
    TODO:
    - [ ] Figure out what to do with the CSS
    - [ ] Stop clipping from happening on mobile

--- astro.config.ts
+++ astro.config.ts
@@ -4,6 +4,9 @@ import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 import purgecss from "astro-purgecss";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { fromHtml } from 'hast-util-from-html'
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,6 +14,7 @@ export default defineConfig({
 	integrations: [mdx(), astroI18next(), sitemap(), compress(), purgecss()],
 	markdown: {
 		syntaxHighlight: "prism",
+		rehypePlugins: [[rehypeAutolinkHeadings, { content: fromHtml('<span class="header-anchor-container icon pr-3"><i class="header-anchor fas fa-lg fa-link has-text-grey-dark is-size-5"></i></span>'), behavior: "prepend" }]],
 	},
 	vite: {
 		build: {
