72982e4fbe9b9ae6dd2694acb8efd43f96de3b36
Ambre Bertucci <ambre@starchild.systems>
2023-03-12 18:30:35 +0100

    Teams: Github sync
    This makes the website pool Github for team assignations. It also makes use of this feature on the applicable teams.

--- scripts/paths.mjs
+++ scripts/paths.mjs
@@ -1,5 +1,6 @@
 export const CACHE_DIR = "./cache/";
 export const PK_CACHE_FILE = CACHE_DIR + "pk.json";
+export const TEAM_INFO_CACHE_FILE = CACHE_DIR + "teamInfo.json";
 
 export const CSS_LOCATION = process.env.CSS_LOCATION || "./node_modules/quilt-bulma/dist";
 export const CSS_FINAL_LOCATION = "./public/assets/css";
