b5678b272c585ebfaefc640943b1600de05e1bf0
Southpaw <git@southpaw1496.dev>
2023-12-03 17:52:33 +0000

    Ongoing experiments

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import astroI18next from "astro-i18next";
 import sitemap from "@astrojs/sitemap";
 import purgecss from "astro-purgecss";
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { fromHtml } from 'hast-util-from-html';
+import astroI18next from "astro-i18next";
 
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://quiltmc.org",
-  integrations: [mdx(), astroI18next(), sitemap(), purgecss(), compress()],
+  integrations: [mdx(), sitemap(), purgecss(), compress(), astroI18next()],
   markdown: {
     syntaxHighlight: "prism",
     rehypePlugins: [[rehypeAutolinkHeadings, {
