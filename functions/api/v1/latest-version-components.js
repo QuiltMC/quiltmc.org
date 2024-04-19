dbda89bc63cada8a7b13977b94db975627be7105
simpledemon <153544772+simpledemonn@users.noreply.github.com>
2024-04-19 15:41:30 +0200

    Correctly sort versions

--- functions/api/v1/latest-version-components.js
+++ functions/api/v1/latest-version-components.js
@@ -1,4 +1,5 @@
 import { xml2js } from "xml-js";
+import * as semverSort from "semver-sort";
 
 const META_GAME_VERSIONS = "https://meta.quiltmc.org/v3/versions/game"
 const MAVEN = "https://maven.quiltmc.org/repository/release/org/quiltmc/"
@@ -19,26 +20,17 @@ export async function onRequest(context) {
 	const loomVersion = (await fetchMetadata("loom")).versioning.latest._text
 	const loaderVersion = (await fetchMetadata("quilt-loader")).versioning.latest._text
 
-	const mappingsMetadata = await fetchMetadata("quilt-mappings")
-	const qfapiMetadata = await fetchMetadata("quilted-fabric-api/quilted-fabric-api")
+	const mappingsVersions = (await fetchMetadata("quilt-mappings"))
+		.versioning.versions.version.map((versioning) => versioning._text)
+		.sort((a, b) => b.split(".").slice(-1) - a.split(".").slice(-1))
+
+	const qfapiVersions = semverSort.asc((await fetchMetadata("quilted-fabric-api/quilted-fabric-api"))
+		.versioning.versions.version.map((versioning) => versioning._text))
+		.toReversed()  // semverSort.desc chokes on those inputs
 
 	for (const { version } of gameVersions) {
-		let mappingsVersion = null
-		let qfapiVersion = null
-
-		for (const versioning of mappingsMetadata.versioning.versions.version) {
-			if (versioning._text.split("+")[0] === version) {
-				mappingsVersion = versioning._text
-				break
-			}
-		}
-
-		for (const versioning of qfapiMetadata.versioning.versions.version) {
-			if (versioning._text.split("-").at(-1) === version) {
-				qfapiVersion = versioning._text
-				break
-			}
-		}
+		const mappingsVersion = mappingsVersions.find((versioning) => versioning.split("+")[0] === version) || null
+		const qfapiVersion = qfapiVersions.find((versioning) => versioning.split("-").at(-1) === version) || null
 
 		output[version] = {
 			"quilt_loader": loaderVersion,
