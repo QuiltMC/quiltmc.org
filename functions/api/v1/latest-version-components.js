0d1352b706c8a98182be6ec5066ad6147a0cdbf0
Up <10714589+UpcraftLP@users.noreply.github.com>
2024-04-24 21:05:29 +0200

    attempt to fix version sorting (#205)
    * rearrange version catalogue order
    * remove external import utility links
    * output errors in worker script

--- functions/api/v1/latest-version-components.js
+++ functions/api/v1/latest-version-components.js
@@ -1,44 +1,66 @@
 import { xml2js } from "xml-js";
-import * as semverSort from "semver-sort";
+import semverSort from "semver-sort";
 
 const META_GAME_VERSIONS = "https://meta.quiltmc.org/v3/versions/game"
 const MAVEN = "https://maven.quiltmc.org/repository/release/org/quiltmc/"
 
 async function fetchMetadata(path) {
-	const metadataUrl = MAVEN + path + "/maven-metadata.xml"
-	const metadataRequest = await fetch(metadataUrl, { headers: { "User-Agent": "QuiltMC Website API" } })
+	const metadataUrl = MAVEN + path + "/maven-metadata.xml";
+	const metadataRequest = await fetch(metadataUrl, { headers: { "User-Agent": "QuiltMC Website API" } });
 
-	return xml2js(await metadataRequest.text(), { compact: true, ignoreAttributes: true }).metadata
+	if (metadataRequest.ok) {
+		return await metadataRequest.text();
+	}
+
+	throw new Error(`Failed to fetch metadata from ${metadataUrl}`);
+}
+
+function getLatestVersion(versions) {
+	// handle null, undefined, empty array
+	if (!versions) return null;
+
+	return semverSort.asc(versions).toReversed()[0];
+}
+
+function getLatestVersionFromMavenMetadataXML(metaXML, latestVersionSelector) {
+	const json = xml2js(metaXML, { compact: true, ignoreAttributes: true });
+
+	const allVersions = json.metadata.versioning.versions.version.map((version) => version._text);
+
+	return latestVersionSelector(allVersions);
 }
 
 export async function onRequest(context) {
-	const gameVersionsRequest = await fetch(META_GAME_VERSIONS, { headers: { "User-Agent": "QuiltMC Website API" } })
-	const gameVersions = await gameVersionsRequest.json()
+	try {
+		const gameVersionsRequest = await fetch(META_GAME_VERSIONS, { headers: { "User-Agent": "QuiltMC Website API" } })
+		const gameVersions = await gameVersionsRequest.json()
 
-	const output = {}
+		const output = {}
 
-	const loomVersion = (await fetchMetadata("loom")).versioning.latest._text
-	const loaderVersion = (await fetchMetadata("quilt-loader")).versioning.latest._text
+		const loomVersion = await fetchMetadata("loom").then(meta => getLatestVersionFromMavenMetadataXML(meta, getLatestVersion));
+		const loaderVersion = await fetchMetadata("quilt-loader").then(meta => getLatestVersionFromMavenMetadataXML(meta, getLatestVersion));
 
-	const mappingsVersions = (await fetchMetadata("quilt-mappings"))
-		.versioning.versions.version.map((versioning) => versioning._text)
-		.sort((a, b) => b.split(".").slice(-1) - a.split(".").slice(-1))
+		const mappingsVersions = await fetchMetadata("quilt-mappings").then(meta => getLatestVersionFromMavenMetadataXML(meta, versions => {
+			return versions.sort((a, b) => b.split(".").slice(-1) - a.split(".").slice(-1));
+		}));
 
-	const qfapiVersions = semverSort.asc((await fetchMetadata("quilted-fabric-api/quilted-fabric-api"))
-		.versioning.versions.version.map((versioning) => versioning._text))
-		.toReversed()  // semverSort.desc chokes on those inputs
+		const qfapiVersions = await fetchMetadata("quilted-fabric-api/quilted-fabric-api").then(meta => getLatestVersionFromMavenMetadataXML(meta, versions => semverSort.asc(versions).toReversed()));
 
-	for (const { version } of gameVersions) {
-		const mappingsVersion = mappingsVersions.find((versioning) => versioning.split("+")[0] === version) || null
-		const qfapiVersion = qfapiVersions.find((versioning) => versioning.split("-").at(-1) === version) || null
+		for (const { version } of gameVersions) {
+			const mappingsVersion = mappingsVersions.find((versioning) => versioning.split("+")[0] === version) || null
+			const qfapiVersion = qfapiVersions.find((versioning) => versioning.split("-").at(-1) === version) || null
 
-		output[version] = {
-			"quilt_loader": loaderVersion,
-			"loom": loomVersion,
-			"quilt_mappings": mappingsVersion,
-			"quilted_fabric_api": qfapiVersion
+			output[version] = {
+				"quilt_loader": loaderVersion,
+				"loom": loomVersion,
+				"quilt_mappings": mappingsVersion,
+				"quilted_fabric_api": qfapiVersion
+			}
 		}
-	}
 
-	return new Response(JSON.stringify(output), { status: 200, headers: { "Content-Type": "text/json" } })
+		return new Response(JSON.stringify(output), { status: 200, headers: { "Content-Type": "text/json" } });
+	}
+	catch (error) {
+		return new Response(JSON.stringify({ error: error.message }), { status: 500, headers: { "Content-Type": "text/json" } });
+	}
 }
