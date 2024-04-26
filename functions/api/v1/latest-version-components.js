49fc80d2eecf6455ce4fadda88d3df1c60465c7a
Up <upcraftedlp@gmail.com>
2024-04-26 22:55:21 +0200

    re-add fetch-depth and fix api response MIME type

--- functions/api/v1/latest-version-components.js
+++ functions/api/v1/latest-version-components.js
@@ -58,9 +58,9 @@ export async function onRequest(context) {
 			}
 		}
 
-		return new Response(JSON.stringify(output), { status: 200, headers: { "Content-Type": "text/json" } });
+		return new Response(JSON.stringify(output), { status: 200, headers: { "Content-Type": "application/json" } });
 	}
 	catch (error) {
-		return new Response(JSON.stringify({ error: error.message }), { status: 500, headers: { "Content-Type": "text/json" } });
+		return new Response(JSON.stringify({ error: error.message }), { status: 500, headers: { "Content-Type": "application/json" } });
 	}
 }
