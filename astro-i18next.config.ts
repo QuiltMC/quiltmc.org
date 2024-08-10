399d5fd588e00f620085aefd510d80a7b3d5e716
Southpaw <git@southpaw1496.dev>
2024-08-10 09:20:59 +0100

    Update i18next config to work in newer versions of astro-i18next

--- astro-i18next.config.ts
+++ astro-i18next.config.ts
@@ -1,15 +1,15 @@
 import type { AstroI18nextConfig } from "astro-i18next";
 
 export default {
-	defaultLanguage: "en",
-	supportedLanguages: ["en"],
-	i18next: {
-		//debug: true, // convenient during development to check for missing keys
+	defaultLocale: "en",
+	locales: ["en"],
+	i18nextServer: {
+		// debug: true, // convenient during development to check for missing keys
 		defaultNS: "common",
 		ns: ["common", "footer", "home", "install", "team"],
 	},
-	i18nextPlugins: {
-		backend: "i18next-fs-fluent-backend",
+	i18nextServerPlugins: {
+		Backend: "i18next-fs-fluent-backend",
 		fluent: "i18next-fluent",
 	},
 } as AstroI18nextConfig;
