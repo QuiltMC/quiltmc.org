927d99a86fdcb8e5f6936e0dd47dfb8850410830
Southpaw <git@southpaw1496.dev>
2024-09-16 18:39:59 +0100

    Change clean script to remove the entire cache directory
    (Fix #252) [skip ci]

--- scripts/clean.mjs
+++ scripts/clean.mjs
@@ -1,5 +1,5 @@
 import * as fs from "fs";
 import { PK_CACHE_FILE } from "./paths.mjs";
 
-console.log("clean: removing PluralKit cache");
-fs.rmSync(PK_CACHE_FILE);
+console.log("clean: Remove cache files");
+fs.rmSync("./cache", {recursive: true});
