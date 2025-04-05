f4c907203912c862a5074b201d9b3df4359829d3
Southpaw <git@southpaw1496.dev>
2025-04-05 12:31:22 +0100

    Cloudflare Hybrid Rendering for the modern era

--- astro.config.ts
+++ astro.config.ts
@@ -6,10 +6,8 @@ import purgecss from "astro-purgecss";
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { fromHtml } from 'hast-util-from-html';
 import icon from "astro-icon"
-
 import compress from "astro-compress";
 import searchIndex from "./src/integration/search-index";
-
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
@@ -39,11 +37,11 @@ export default defineConfig({
 	},
 	ssr: {
 		// These are all Node.js APIs that don't work on Cloudflare. They are only used at build-time, but Astro doesn't understand that they won't be needed in the Cloudflare environment, so they have to be explicitly allowed.
-		external: ["child_process", "crypto", "fs", "module", "node:buffer", "node:child_process", "node:events", "node:fs", "node:os", "node:process", "node:path", "node:stream", "node:stream/promises", "node:string_decoder", "node:timers/promises", "node:url", "node:util", "node:tty", "node:v8", "os", "path", "stream", "string_decoder", "tty", "url", "util", "worker_threads","process"]
+		external: ["child_process", "crypto", "fs", "module", "node:buffer", "node:child_process", "node:events", "node:fs", "node:os", "node:process", "node:path", "node:stream", "node:stream/promises", "node:string_decoder", "node:timers/promises", "node:url", "node:util", "node:tty", "node:v8", "os", "path", "stream", "string_decoder", "tty", "url", "util", "worker_threads","process", "execa"]
 	}
 	},
 
-  output: "hybrid",
+  output: "static",
   adapter: cloudflare({
 	platformProxy: {
 		enabled: true // Enables bindings, secrets, etc. in Astro's dev server
