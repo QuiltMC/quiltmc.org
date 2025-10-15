397973a1de4fc9676238ca33e0f2f103995d5d62
Southpaw <44805409+Southpaw1496@users.noreply.github.com>
2025-10-15 18:05:57 +0100

    Migrate API routes from Cloudflare Functions to Astro's SSR system (#263)
    * Move `api.astro` to folder in src/pages.
    
    * Add Astro Cloudflare adapter
    
    * Format Astro config file
    
    * Switch to hybrid rendering (pre-rendered by default)
    
    * Move all API functions to Astro endpoints
    
    * Replace fast-xml-parser and semver-sort with lighter alternatives
    
    They are the only dependencies needed at runtime, so it’s important to keep them on the lighter side when possible. They are also better-maintained and have fewer dependencies than the outgoing packages.
    
    * Allow Node.js APIs required by various build-time integrations.
    
    I *think* the run time ones are all Node-free, but Cloudflare’s Node.js compatibility layer should catch them.
    
    * I'm not actually sure what this commit does.
    
    * Cloudflare Hybrid Rendering for the modern era
    
    * Update ARCHITECTURE.md to reflect moving of API files
    
    * Remove the awful hack for bundling packages at publish time.
    
    * Use the compile image service for compression
    
    Cloudflare doesn’t support the default sharp service

--- astro.config.ts
+++ astro.config.ts
@@ -6,15 +6,16 @@ import purgecss from "astro-purgecss";
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { fromHtml } from 'hast-util-from-html';
 import icon from "astro-icon"
-
 import compress from "astro-compress";
 import searchIndex from "./src/integration/search-index";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://quiltmc.org/",
-	integrations: [
-		mdx(),
+  site: "https://quiltmc.org/",
+
+  integrations: [
+      mdx(),
 	astroI18next(),
 	sitemap(),
 	purgecss({ safelist: ["has-background-info", "has-background-link"] }),
@@ -22,15 +23,29 @@ export default defineConfig({
 	searchIndex(),
 	icon()
 	],
-	markdown: {
+
+  markdown: {
 	syntaxHighlight: "prism",
 	rehypePlugins: [[rehypeAutolinkHeadings, {
-		behavior: "append"
+      behavior: "append"
 	}]]
 	},
-	vite: {
+
+  vite: {
 	build: {
-		assetsInlineLimit: 0
+      assetsInlineLimit: 0
+	},
+	ssr: {
+		// These are all Node.js APIs that don't work on Cloudflare. They are only used at build-time, but Astro doesn't understand that they won't be needed in the Cloudflare environment, so they have to be explicitly allowed.
+		external: ["child_process", "crypto", "fs", "module", "node:buffer", "node:child_process", "node:events", "node:fs", "node:os", "node:process", "node:path", "node:stream", "node:stream/promises", "node:string_decoder", "node:timers/promises", "node:url", "node:util", "node:tty", "node:v8", "os", "path", "stream", "string_decoder", "tty", "url", "util", "worker_threads","process", "execa"]
 	}
-	}
+	},
+
+  output: "static",
+  adapter: cloudflare({
+	platformProxy: {
+		enabled: true // Enables bindings, secrets, etc. in Astro's dev server,
+	},
+	imageService: "compile"
+  }),
 });
